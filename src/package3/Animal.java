4cbf397c9ad4fe0764c1660d778cf8fbd61a85ef
Ruslan Zapolianenko <rusartrade@gmail.com>
2022-09-13 22:35:21 +0300

    Homework5

--- src/package3/Animal.java
+++ src/package3/Animal.java
@@ -1,2 +1,10 @@
-package package3;public class Animal {
+package package3;
+
+public class Animal {
+    private int field_1 = 1;
+    private String field_2 = "field_2";
+    public int field_3 = 3;
 }
+//Создайте 2 класса, Animal и Cat.
+//В классе Animal инициализируйте 3 поля различных модификаторов.
+//В классе Cat используя рефлексию получите доступ к полям класса Animal и измените содержимое каждого из полей
--- src/package3/Cat.java
+++ src/package3/Cat.java
@@ -1,4 +1,38 @@
 package package3;
 
-public class Main {
+import java.lang.reflect.Field;
+
+public class Cat {
+    public static void main(String[] args) {
+        try {
+            Class<?> cl = Animal.class;
+            Animal animalO = new Animal();
+
+            Field field_1 = cl.getDeclaredField("field_1");
+            Field field_2 = cl.getDeclaredField("field_2");
+            Field field_3 = cl.getDeclaredField("field_3");
+
+            field_1.setAccessible(true);
+            field_2.setAccessible(true);
+
+            System.out.println("field_1: " + field_1.getInt(animalO));
+            field_1.setInt(animalO, 10);
+            System.out.println(" - New field_1: " + field_1.getInt(animalO));
+            System.out.println("______________________________");
+            System.out.println("field_2 String: " + field_2.get(animalO));
+            field_2.set(animalO, "String String String");
+            System.out.println("- New field_2 String: " + field_2.get(animalO));
+            System.out.println("______________________________");
+            System.out.println("field_3: " + field_3.getInt(animalO));
+            field_3.setInt(animalO, 333333);
+            System.out.println(" - New field_3: " + field_3.getInt(animalO));
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
+//Создайте 2 класса, Animal и Cat.
+//В классе Animal инициализируйте 3 поля различных модификаторов.
+//В классе Cat используя рефлексию получите доступ к полям класса Animal и измените содержимое каждого из полей
